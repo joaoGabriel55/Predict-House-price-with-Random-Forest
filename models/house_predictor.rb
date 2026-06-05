be9ba31711b7577509f565eef132b2c887857b63
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2026-06-05 10:56:14 -0300

    fix: Use correct types for predictor inputs
    Cast construction_year and zipcode as integers instead of floats,
    and clamp calc_acres below 1 to 0.0. Remove redundant .to_f
    calls in HousePredictor#predict for fields already cast by caller.

--- models/house_predictor.rb
+++ models/house_predictor.rb
@@ -12,12 +12,12 @@ class HousePredictor
 
   def predict(construction_year:, lot_sqft:, calc_acres:, land_value:, improvement_value:, zipcode:)
     input = Numo::DFloat[[
-      construction_year.to_f,
+      construction_year,
       lot_sqft.to_f,
       calc_acres.to_f,
       land_value.to_f,
       improvement_value.to_f,
-      zipcode.to_f
+      zipcode
     ]]
     @model.predict(input)[0].round(2)
   end
--- models/house_predictor_llm.rb
+++ models/house_predictor_llm.rb
@@ -6,22 +6,24 @@ class HousePredictorLLM < RubyLLM::Tool
   desc "Predicts Las Vegas house price based on construction year, lot square feet, calculated acres, land value, improvement value, and zipcode"
 
   def execute(construction_year:, lot_sqft:, calc_acres:, land_value:, improvement_value:, zipcode:)
+    _calc_acres = calc_acres.to_f < 1 ? 0.0 : calc_acres.to_f
+
     price = HousePredictor.instance.predict(
-      construction_year: construction_year.to_f,
+      construction_year: construction_year.to_i,
       lot_sqft: lot_sqft.to_f,
-      calc_acres: calc_acres.to_f,
+      calc_acres: _calc_acres,
       land_value: land_value.to_f,
       improvement_value: improvement_value.to_f,
-      zipcode: zipcode.to_f
+      zipcode: zipcode.to_i
     )
 
     {
-      construction_year: construction_year.to_f,
+      construction_year: construction_year.to_i,
       lot_sqft: lot_sqft.to_f,
-      calc_acres: calc_acres.to_f,
+      calc_acres: _calc_acres,
       land_value: land_value.to_f,
       improvement_value: improvement_value.to_f,
-      zipcode: zipcode.to_f,
+      zipcode: zipcode.to_i,
       predicted_price: price
     }
   rescue => e
